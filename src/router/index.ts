8c231e38cdc98d16efdf9df7d0534d27125a35d3
wbzf <2428935270@qq.com>
2021-01-15 16:49:52 +0800

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,45 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import VueRouter from 'vue-router'
+import Home from '../views/First.vue'
+import Second from  '../views/Second.vue'
+import EmpiricalCalculation from "@/components/EmpiricalCalculation.vue";
+import Test from "@/components/Test.vue";
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = [
+const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children:[
+        {
+          path: '',
+          component: EmpiricalCalculation
+        },
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Second',
+    name: 'Second',
+    component: Second,
+    children: [
+        {
+          path: '/',
+          component: Test
+    }
+    ]
+  },
+  {
+    path: '/Third',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Third.vue')
+  },
+  {
+    path: '/Fourth',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Fourth.vue')
+  },
+  {
+    path: '/Second/Test',
+    component: Test
   }
 ]
 
